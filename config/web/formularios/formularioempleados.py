29655bfd111b97cdca8caa5166714c74fd91651b
MacBast <danielbastidas459@gmail.com>
2022-10-29 13:07:01 -0500

    guardando avance NTS

---
+++ config/web/formularios/formularioempleados.py
@@ -0,0 +1,40 @@
+from django import forms
+
+class FormularioEmpleados (forms.Form):
+
+    CARGO=(
+        (1,'cheff'),
+        (2,'administrados'),
+        (3,'mesero'),
+        (3,'ayudante'),
+    )
+
+    nombre=forms.CharField(
+        required=True,
+        max_length=5,
+        label='Nombre Empleado: ',
+        widget=forms.TextInput(attrs={'class': 'form-control mb-3'})
+    )
+    apellidos=forms.CharField(
+        required=True,
+        max_length=5,
+        widget=forms.TextInput(attrs={'class': 'form-control mb-3'})
+    )
+    fotografia=forms.CharField(
+        required=True,
+        widget=forms.TextInput(attrs={'class': 'form-control mb-3'})
+    )
+    cargo=forms.ChoiceField(
+        required=True,
+        widget=forms.Select(attrs={'class': 'form-control mb-3'}),
+        choices=CARGO
+    )
+    salario=forms.CharField(
+        required=True,
+        max_length=20,
+        widget=forms.NumberInput(attrs={'class': 'form-control mb-3'})
+    )
+    contacto=forms.CharField(
+        required=True,
+        widget=forms.TextInput(attrs={'class': 'form-control mb-3'})
+    )
--- config/web/views.py
+++ config/web/views.py
@@ -1,7 +1,30 @@
+from importlib import import_module
 from django.shortcuts import render
+#from config.web.formularios.formularioempleados import FormularioEmpleados
+
+from web.formularios.formularioplatos import FormularioPlatos
+from web.formularios.formularioempleados import FormularioEmpleados
 
 # Create your views here.
 #TODAS LAS VISTAS SON FUNCIONES DE PYTHON
 
 def Home(request):
     return render(request,'home.html')
+
+def Platos(request):
+    #Esta vista utilizara un formulario django
+    #Debor crear un objeto de la clase FormularioPlatos()
+    formulario=FormularioPlatos()
+
+    #Creamos un diccionario para enviar el formulario al HTML(template)
+    data={
+        'formulario': formulario
+    }
+    return render(request, 'menuplatos.html', data)
+
+def Empleados(request):
+    formularioe=FormularioEmpleados()
+    data={
+        'formularioe': formularioe
+    }
+    return render(request, 'empleados.html', data)
